e3a094de42084249373c9ea5597a0cd9f70398fd
Caleb <you@example.cfongiam20@gmail.com>
2024-12-03 23:48:30 +0100

    adding jenkinsfile

--- ec2.tf
+++ ec2.tf
@@ -9,15 +9,3 @@ resource "aws_instance" "airbnb-web" {
     Name = "airbnb-web-server"
   }
 }
-
-resource "aws_instance" "airbnb-web" {
-  ami           =var.ami_id
-  instance_type = var.instance_type
-  monitoring    = true
-  ebs_optimized = true
-  
-   
-  tags = {
-    Name = "airbnb-web-server"
-  }
-}
